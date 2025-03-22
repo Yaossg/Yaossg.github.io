123301041b82d386126bbdeeba3b7cc6cb57add6
Yaossg <Yaossg@users.noreply.github.com>
2025-03-22 06:15:20 +0000

    deploy: Yaossg/site@665ede5f4b34c56ce1847bba31d94299dc172292

---
+++ site/assets/js/617d5fa8.638c4a54.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([[4623],{38200:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/site/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":10,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
